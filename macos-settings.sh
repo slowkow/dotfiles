a2ef45200be8198bdf6e3420749ebd2c4ff70dd0
Kamil Slowikowski <kslowikowski@gmail.com>
2018-04-06 09:44:57 -0400

    enable more macos settings

--- macos-settings.sh
+++ macos-settings.sh
@@ -219,10 +219,10 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # 
 # # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
 # defaults write com.apple.finder QuitMenuItem -bool true
-# 
-# # Finder: disable window animations and Get Info animations
-# defaults write com.apple.finder DisableAllAnimations -bool true
-# 
+ 
+# Finder: disable window animations and Get Info animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
 # # Set Desktop as the default location for new Finder windows
 # # For other paths, use `PfLo` and `file:///full/path/here/`
 # defaults write com.apple.finder NewWindowTarget -string "PfDe"
@@ -233,28 +233,28 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 # defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 # defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-# 
-# # Finder: show hidden files by default
-# #defaults write com.apple.finder AppleShowAllFiles -bool true
-# 
-# # Finder: show all filename extensions
-# defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-# 
-# # Finder: show status bar
-# defaults write com.apple.finder ShowStatusBar -bool true
-# 
-# # Finder: show path bar
-# defaults write com.apple.finder ShowPathbar -bool true
-# 
-# # Display full POSIX path as Finder window title
-# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-# 
-# # Keep folders on top when sorting by name
-# defaults write com.apple.finder _FXSortFoldersFirst -bool true
-# 
-# # When performing a search, search the current folder by default
-# defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-# 
+ 
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+ 
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+ 
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+ 
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 # # Disable the warning when changing a file extension
 # defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 # 
@@ -300,20 +300,20 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
-# 
-# # Use list view in all Finder windows by default
-# # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-# defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-# 
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
 # # Disable the warning before emptying the Trash
 # defaults write com.apple.finder WarnOnEmptyTrash -bool false
 # 
 # # Enable AirDrop over Ethernet and on unsupported Macs running Lion
 # defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-# 
-# # Show the ~/Library folder
-# chflags nohidden ~/Library
-# 
+ 
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
 # # Show the /Volumes folder
 # sudo chflags nohidden /Volumes
 # 
@@ -428,21 +428,21 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # ###############################################################################
 # # Safari & WebKit                                                             #
 # ###############################################################################
-# 
-# # Privacy: don’t send search queries to Apple
-# defaults write com.apple.Safari UniversalSearchEnabled -bool false
-# defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-# 
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
 # # Press Tab to highlight each item on a web page
 # defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true
 # defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool true
-# 
-# # Show the full URL in the address bar (note: this still hides the scheme)
-# defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
-# 
-# # Set Safari’s home page to `about:blank` for faster loading
-# defaults write com.apple.Safari HomePage -string "about:blank"
-# 
+
+# Show the full URL in the address bar (note: this still hides the scheme)
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+# Set Safari’s home page to `about:blank` for faster loading
+defaults write com.apple.Safari HomePage -string "about:blank"
+
 # # Prevent Safari from opening ‘safe’ files automatically after downloading
 # defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 # 
@@ -745,10 +745,10 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # ###############################################################################
 # # Photos                                                                      #
 # ###############################################################################
-# 
-# # Prevent Photos from opening automatically when devices are plugged in
+ 
+# Prevent Photos from opening automatically when devices are plugged in
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-# 
+ 
 # ###############################################################################
 # # Messages                                                                    #
 # ###############################################################################
