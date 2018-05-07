98d7bf0c2b0d3eb1a93d8f69cba5407de87b7684
Kamil Slowikowski <kslowikowski@gmail.com>
2018-05-07 14:14:27 -0400

    change screenshot directory

--- macos-settings.sh
+++ macos-settings.sh
@@ -197,12 +197,12 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (2
 # defaults write com.apple.screensaver askForPassword -int 1
 # defaults write com.apple.screensaver askForPasswordDelay -int 0
 # 
-# # Save screenshots to the desktop
-# defaults write com.apple.screencapture location -string "${HOME}/Desktop"
-# 
-# # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-# defaults write com.apple.screencapture type -string "png"
-# 
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Dropbox/Screenshots"
+ 
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
 # # Disable shadow in screenshots
 # defaults write com.apple.screencapture disable-shadow -bool true
 # 
