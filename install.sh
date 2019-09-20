f7fbe3bf44ec91ce77c12c567448ad7160a3b0eb
Kamil Slowikowski <kslowikowski@gmail.com>
2019-09-20 11:18:47 -0400

    reorganize, change rust packages

--- install.sh
+++ install.sh
@@ -28,20 +28,14 @@ brew autoupdate --start --upgrade --cleanup --enable-notifications
 
 # Learn more about any package at: http://brewformulas.org/PACKAGE
 
-# Search for a string across many files
-brew install ripgrep
-# brew install ack
-# brew install the_platinum_searcher
-# brew install the_silver_searcher
-
-# Utilities like `sponge`.
-brew install moreutils --without-parallel
-
 # Utilities for compiling C code.
 brew install cmake
 brew install coreutils
 
-# Install Bash 4.
+# Utilities like `sponge`.
+#brew install moreutils --without-parallel
+
+# Install latest Bash
 brew install bash
 brew install bash-completion2
 # Switch to using brew-installed bash as default shell
@@ -53,35 +47,39 @@ fi
 
 # New version of vim
 # Note: this installs sqlite, perl, python3
-brew install vim --with-override-system-vi
+brew install vim #--with-override-system-vi
 pip3 install --upgrade pip setuptools wheel
 
+# Search for a string across many files
+# brew install ripgrep # (use cargo to install ripgrep)
+# brew install ack
+# brew install the_platinum_searcher
+# brew install the_silver_searcher
+
 # Get the latest Python2
-brew install python@2
-pip install --upgrade pip setuptools
+#brew install python@2
+#pip install --upgrade pip setuptools
 
 # Git version control
 brew install git
 brew install git-lfs
-brew install git-annex
+# brew install git-annex
 
 # Delete files from a git repo history
 brew install bfg
 
-# Ruby Version Manager
-gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-rvm install ruby-head
-gem install bundler
+# # Ruby Version Manager
+# gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# curl -sSL https://get.rvm.io | bash -s stable
+# rvm install ruby-head
+# gem install bundler
+
 
 #####################################################################
 # Other Stuff
 
-# Alternative to `cat`
-brew install bat
-
 # Needed to compile older versions of Karabiner Elements
-brew install boost
+# brew install boost
 
 # Alternative to `diff`
 brew install diff-so-fancy
@@ -89,26 +87,15 @@ brew install diff-so-fancy
 # Convert line endings
 brew install dos2unix
 
-# Alternative to `ls`
-brew install exa
-
-# Alernative to `find`
-brew install fd
-
 # View images quickly
 brew install feh
 
 # Convert video files
 brew install ffmpeg
 
-# User-friendly alternative to Bash
-brew install fish
-# Switch to using fish as default shell
-if ! fgrep -q '/usr/local/bin/fish' /etc/shells
-then
-  echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
-  # chsh -s /usr/local/bin/fish
-fi
+# Lots of weird icons and glyphs
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
 
 # Fuzzy search, useful for history
 brew install fzy
@@ -153,37 +140,43 @@ brew install jump
 brew install jq
 
 # MP3 encoding
-brew install lame
+# brew install lame
 
 # Mac App Store Command Line Interface
-brew install mas
+# brew install mas
 
 # Amazon app for reading ebooks
-mas 'Kindle', id: 405399194
+# mas 'Kindle', id: 405399194
 
 # Visualize disk space usage
-mas 'GrandPerspective', id: 1111570163
+# mas 'GrandPerspective', id: 1111570163
 
 # Resize windows neatly
-mas 'Magnet', id: 441258766
+# mas 'Magnet', id: 441258766
 
 # Version control with hg
-brew install mercurial
+# brew install mercurial
 
 # like sed, awk, cut, join, and sort for name-indexed data such as CSV or JSON
-brew install miller
+# brew install miller
 
 # Alternative to ssh for poor quality internet connections
 brew install mosh
 
+# Compress JPEG files
+# brew install mozjpeg
+
 # Alternative to unison for synchronizing filesystems
-brew install havoc-io/mutagen/mutagen
+# brew install havoc-io/mutagen/mutagen
 
 # Alternative to `du`
 brew install ncdu
 
 # New version of vim
 brew install neovim
+
+# nim programming language
+# brew install nim
 
 # New version of ssh
 brew install openssh
@@ -193,31 +186,31 @@ brew install pandoc
 brew install pandoc-citeproc
 
 # Convert PDF to HTML
-brew install pdf2htmlEX
+# brew install pdf2htmlEX
 
 # GNU parallel executes jobs concurrently locally or using remote computers
-brew install parallel
+# brew install parallel
 
 # Parallel gzip
-brew install pigz
+# brew install pigz
 
 # Optimize PNG files
 brew install pngquant
 
 # Compute dissimilarity between two or more PNG files
-brew install dssim
+# brew install dssim
 
 # Run SQL commands on CSV files: q "SELECT COUNT(*) FROM file.csv where c3 > 1"
-brew install q
+# brew install q
 
 # Maybe needed for tmux?
 # brew install reattach-to-user-namespace
 
 # Quickly rename files with rules
-brew install rename
+# brew install rename
 
 # Compile SASS to CSS
-brew install sassc
+# brew install sassc
 
 # SQLite is a lightweight alternative to MySQL
 brew install sqlite
@@ -240,12 +233,11 @@ brew install watch
 # Alternative to curl, download files from the web
 brew install wget
 
+# List of TSV tools: https://github.com/eBay/tsv-utils/blob/master/docs/OtherToolkits.md
+
 # Compress files very efficiently, but slowly
 brew install xz
 
-# Node.js package manager
-# Note: Installs node
-brew install yarn
 
 #####################################################################
 # The R-Project for Statistical Computing
@@ -259,7 +251,10 @@ brew cask install java
 brew install openblas
 
 # R language for statistical computing
-brew install r --with-openblas --with-java
+# brew install r --with-openblas --with-java --with-cairo
+
+# The R version from brew cannot be compiled with cairo.
+# So, it may be better to get it from CRAN: https://cloud.r-project.org/bin/macosx/R-3.5.1.pkg
 
 # LaTeX tools like pdflatex
 brew cask install basictex
@@ -282,3 +277,40 @@ brew install mariadb-connector-c
 # Needed for R packages: udunits2, units, ggforce
 brew install udunits
 
+
+#####################################################################
+# Node.js programs
+
+# Node.js package manager (Note: Installs node)
+brew install yarn
+
+# Optimize SVG files
+npm install -g svgo
+
+
+#####################################################################
+# Rust
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Alternative to sed
+cargo install sd
+
+# Alternative to `grep` (`rg`)
+cargo install ripgrep
+
+# A fast CSV command line toolkit written in Rust.
+cargo install xsv
+
+# Alternative to `cat`
+# brew install bat
+cargo install bat
+
+# Alternative to `ls`
+# brew install exa
+cargo install exa
+
+# Alernative to `find`
+# brew install fd
+cargo install fd-find
